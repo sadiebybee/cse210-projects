18421dbcdbc388ab28d0fa36a93b0167b17ec12b
sadiebybee <byb22004@byui.edu>
2025-04-23 11:10:15 -0600

    activity1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // This is a program to compute the area of a circle.
+
+        // Get the radius of the circle from the user.
+        Console.Write("Please enter the radius: ");
+        string text = Console.ReadLine();
+        double radius = double.Parse(text);
+
+        //compute the area of the circle.
+        double area = Math.PI * radius * radius;
+
+        // Display the area for the user to see.
+        Console.WriteLine($"Area of the circle: {area}");
     }
 }
