f3716fd7516f452913aaf6a6eb176fb2ac0ee0b8
sadiebybee <byb22004@byui.edu>
2025-04-26 17:39:42 -0600

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+
+        // Prompt the user for their first name. Then, prompt them for their last name.
+        Console.WriteLine("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        // Display the text back all on one line saying
+        Console.WriteLine($"Your name is {lname}, {fname}, {lname}");
     }
 }
