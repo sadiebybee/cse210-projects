6b45c1d528184a781a0cc3594d499cff8850bebf
sadiebybee <byb22004@byui.edu>
2025-05-01 12:08:48 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -33,6 +33,21 @@ class Program
         Console.WriteLine($"The sum is: {sum}");
 
         //Compute the average
-        
+        float average = ((float)sum / numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the max
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
