01fd812b8e137db700d92a83dd623d3d1d7c215e
sadiebybee <byb22004@byui.edu>
2025-06-13 15:53:40 -0600

    stretch

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,5 +1,11 @@
 using System;
 
+// For the stretch challenge, I added a count system that keeps track of 
+// how many times the user does an activity in one session. I then added a
+// summary that is displayed when the user decides to quit the program. The 
+// summary shows the total amount of times the the user did an activity in 
+// the session.
+
 class Program
 {
     static void Main(string[] args)
@@ -7,6 +13,10 @@ class Program
         Console.WriteLine();
         Console.WriteLine("Welcome to the Mindfullness Activity!");
         Console.WriteLine();
+
+        int breathingCount = 0;
+        int reflectingCount = 0;
+        int listingCount = 0;
 
         bool running = true;
         while (running)
@@ -25,6 +35,7 @@ class Program
             switch (choice)
             {
                 case "1":
+                    breathingCount++;
                     BreathingActivity breathing = new BreathingActivity(
                     "Breathing",
                     "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.",
@@ -36,6 +47,7 @@ class Program
                     break;
 
                 case "2":
+                    reflectingCount++;
                     ReflectingActivity reflect = new ReflectingActivity(
                     "Reflection",
                     "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.",
@@ -45,6 +57,7 @@ class Program
                     break;
 
                 case "3":
+                    listingCount++;
                     ListingActivity listing = new ListingActivity(
                     "Listing",
                     "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.",
@@ -56,9 +69,15 @@ class Program
                 case "4":
                     running = false;
                     Console.WriteLine("Goodbye, see you next time!");
+                    Console.WriteLine("\nSession Summary:");
+                    Console.WriteLine($"Breathing Activities completed: {breathingCount}");
+                    Console.WriteLine($"Reflecting Activities completed: {reflectingCount}");
+                    Console.WriteLine($"Listing Activities completed: {listingCount}");
+                    int totalActivities = breathingCount + reflectingCount + listingCount;
+                    Console.WriteLine($"Total Activities: {totalActivities}");
                     Console.WriteLine();
                     break;
-                
+
                 default:
                     Console.WriteLine("Invalid, Please enter a number 1-4.");
                     Console.WriteLine();
