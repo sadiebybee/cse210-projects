ecde9d08b4d68032a4ad2a5c7e3191f2ad109376
sadiebybee <byb22004@byui.edu>
2025-07-19 00:44:51 -0600

    StretchChallenge

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,5 +1,9 @@
 using System;
 
+// For the stretch challenge: I have more than 8 classes and I added a little story to my game.
+// I added a system that keeps track of the players health to make the game more advanced and interesting.
+// I have multiple kinds of puzzles that are also used multiple times during the game.
+// I used Abstract, Encapsulation, Inheritance, and Polymorphism multiple times in multiple ways.
 class Program
 {
     static void Main(string[] args)
